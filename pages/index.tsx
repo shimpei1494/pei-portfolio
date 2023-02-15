3da37abfd8c781279e1ca652f4f3ce5ffcf7d52d
shimpei1494 <one1218all38@icloud.com>
2023-02-15 22:27:05 +0900

    エラー回避

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello Peishim's Portfolio</h1>
+        <h1>Hello Peishim Portfolio</h1>
       </main>
     </>
   )
