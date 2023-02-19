ef489ed1c8ca73d447b4986be8a8546fe89dd3fd
shimpei1494 <one1218all38@icloud.com>
2023-02-19 11:47:12 +0900

    ヘッダーのインデント修正、ヘッダーに隠れる部分の余白をコンポーネント内に追加

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -70,34 +70,36 @@ export default function Header(props: Props) {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-      <AppBar component="nav" color='primary'>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            Peishim Portfolio
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
+        <AppBar component="nav" color='primary'>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Peishim Portfolio
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map((item) => (
+                <Button key={item} sx={{ color: '#fff' }}>
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
       </ThemeProvider>
+      {/* ↓ページコンテンツが最初にappbar分下に行って被らなくなる */}
+      <Toolbar />
       <Box component="nav">
         <Drawer
           container={container}
