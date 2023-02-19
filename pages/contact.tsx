b996c0f8048f1336eb6101ed5036118f35df9b9a
shimpei1494 <one1218all38@icloud.com>
2023-02-19 18:43:25 +0900

    コンタクトページの追加

---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"
+import { HeadTag } from "../components/HeadTag"
+
+function Contact() {
+  return (
+    <>
+    <HeadTag />
+    <main>
+      <Header />
+      <h2>Contact</h2>
+    </main>
+    </>
+  )
+};
+
+export default Contact
