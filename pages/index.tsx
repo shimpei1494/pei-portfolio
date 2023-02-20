684b8aab8517231c1e3b9b67beb37270305a8a7d
shimpei1494 <one1218all38@icloud.com>
2023-02-21 06:57:14 +0900

    トップページ簡単なレイアウト作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { Container } from '@mui/system'
+import { Divider } from '@mui/material'
+import { Box, Container, Stack } from '@mui/system'
 import Header from '../components/Header'
 import { HeadTag } from '../components/HeadTag'
 
@@ -10,8 +11,15 @@ export default function Home() {
         <Header />
         {/* 画像はスマホサイズでいい感じに表示されないようであれば、非表示にする */}
         <img src="/images/top-snowboard3.jpeg" width="100%" alt="Topページのスノーボード画像" />
-        <Container>
-          <h2>hello</h2>
+        <Container maxWidth='md'>
+          <Stack divider={<Divider orientation="horizontal"/>} spacing={2}>
+            <Box>このサイトの説明</Box>
+            <Box>自己紹介とスキル、キャリア</Box>
+            <Box>ブログの紹介</Box>
+            <Box>アプリ紹介</Box>
+            <Box>趣味</Box>
+            <Box>問い合わせ</Box>
+          </Stack>
         </Container>
       </main>
     </>
