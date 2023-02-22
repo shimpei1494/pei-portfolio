cc1263d4f6b0fb0b10cab0c83c6e2973be66a4b7
大下慎平 <one1218all38@icloud.com>
2023-02-22 13:10:02 +0900

    不要部分の削除

--- components/layouts/Header/index.tsx
+++ components/layouts/Header/index.tsx
@@ -12,8 +12,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material';
-import { cyan, indigo } from '@mui/material/colors';
 import NextLink from 'next/link';
 
 interface Props {
