a9525fd43fad746a4ee89321ed255df120309e5b
shimpei1494 <one1218all38@icloud.com>
2023-02-25 17:18:22 +0900

    hobbyのyoutube部分まで仮作成
    レスポンシブ対応はすぐできなかったので後日

--- components/SnowYoutube/index.tsx
+++ components/SnowYoutube/index.tsx
@@ -1,12 +1,16 @@
-import YouTube from "react-youtube";
+import { Stack } from "@mui/material";
+import YouTube, { YouTubeProps } from "react-youtube";
 
-type Props = {
-  url: string
-};
+const  opts : YouTubeProps [ 'opts' ]  =  { 
+  height : '360' , 
+  width : '640' ,
+} ;
 
 export function SnowYoutube() {
 
   return (
-    <YouTube videoId="aFjI7OqTL70" />
+    <Stack alignItems='center' my={2}>
+      <YouTube videoId="aFjI7OqTL70" opts={opts} />
+    </Stack>
   );
 }
