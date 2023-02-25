0d18a038bead0d4efee9238235da15619355d3d9
shimpei1494 <one1218all38@icloud.com>
2023-02-25 16:35:57 +0900

    スノボのyoutubeが表示されるように

---
+++ components/SnowYoutube/index.tsx
@@ -0,0 +1,12 @@
+import YouTube from "react-youtube";
+
+type Props = {
+  url: string
+};
+
+export function SnowYoutube() {
+
+  return (
+    <YouTube videoId="aFjI7OqTL70" />
+  );
+}
--- pages/hobby.tsx
+++ pages/hobby.tsx
@@ -1,7 +1,9 @@
+import { Snowboarding } from "@mui/icons-material"
 import { Box, Container, Typography } from "@mui/material"
 import { Footer } from "../components/layouts/Footer"
 import Header from "../components/layouts/Header"
 import { HeadTag } from "../components/layouts/HeadTag"
+import { SnowYoutube } from "../components/SnowYoutube"
 
 function Hobby() {
   return (
@@ -19,6 +21,14 @@ function Hobby() {
             興味がある人のみ、ゆる〜く見てください。
           </Typography>
         </Box>
+        <Typography variant="h2" align='center' color='secondary' mb={2}>
+          Snowboard <Snowboarding fontSize="large" />
+        </Typography>
+        <Typography align="center">
+          大学時代の6年間で合計170回ほどスノボに行くほど、スノボにハマっていました。<br/>
+          最近では行く回数も減りましたが、昔は動画も作ったりしていました。
+        </Typography>
+        <SnowYoutube />
       </Container>
       <Footer />
     </main>
