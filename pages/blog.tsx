4a22f840394290b6744c6d9aaeba42e12c839aec
大下慎平 <one1218all38@icloud.com>
2023-03-03 17:56:43 +0900

    map処理するkeyの場所を変更

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -47,8 +47,8 @@ function Blog({blogs,tags}: Props) {
             <Box mt={6}>
               <Grid container spacing={4} >
                 {blogs.map((blog) => (
-                  <Grid item xs={12} md={4}>
-                    <NextLink href={blog.url} target="_blank" rel="noopener noreferrer" key={blog.id}>
+                  <Grid item xs={12} md={4} key={blog.id}>
+                    <NextLink href={blog.url} target="_blank" rel="noopener noreferrer">
                       <Card variant="outlined" style={{backgroundColor: "#fff8dc", display: true? "block" : "none"}} >
                         <CardContent>
                           <Typography color='#a9a9a9'>
