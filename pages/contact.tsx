bf6a8e57d474956eb75d5c68a04e3325424063e8
大下慎平 <one1218all38@icloud.com>
2023-04-07 14:14:30 +0900

    リキャプチャのフロント実装

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,6 +8,7 @@ import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import {useRouter} from "next/router";
 import { PageTitle } from "../components/PageTitle";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
 // フォームの型
 type ContactForm = {
@@ -29,12 +30,39 @@ const schema = yup.object({
 
 function Contact() {
   const router = useRouter();
+  const { executeRecaptcha } = useGoogleReCaptcha();
   const { register, handleSubmit, formState: {errors}, } = useForm<ContactForm>({
     resolver: yupResolver(schema),
   });
 
   // フォーム送信時の処理（バリデーションOKな時に実行される）
   const onSubmit: SubmitHandler<ContactForm> = async (data) => {
+    // リキャプチャ
+    if (!executeRecaptcha) {
+      console.log('Execute recaptcha not yet available');
+      alert("reCAPTCHAエラーです。");
+      return;
+    }
+    const reCaptchaToken: String = await executeRecaptcha("Contact");
+    console.log(reCaptchaToken);
+    const serverEndpoint = "api/recaptcha";
+
+    const responce_server = await fetch(serverEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: reCaptchaToken,
+      }),
+    });
+
+    if (responce_server.status === 500) {
+      alert("reCAPTCHAでbot判定されました。再度お試しください。");
+      return
+    }
+
+    // メールを送る
     const response = await fetch("api/sendMail", {
       method: "POST",
       headers: {
@@ -51,34 +79,34 @@ function Contact() {
 
   return (
     <>
-    <HeadTag />
-    <main>
-      <Header />
-      <Container maxWidth='md'>
-        <Box mb={6}>
-          <PageTitle title="Contact." />
-          <Typography align='center'>
-            このページはお問い合わせページです。<br/>
-          </Typography>
-        </Box>
-        <Box>
-          <FormControl fullWidth>
-            <Stack spacing={3} direction='column' >
-              <TextField variant="outlined" required label="お名前" {...register('name')} error={'name' in errors}  helperText={errors.name?.message} />
-              <TextField variant="outlined" required label="メールアドレス" type="email" {...register('email')} error={'email' in errors}  helperText={errors.email?.message} />
-              <TextField variant="outlined" required label="問い合わせ内容" multiline rows={6} {...register('message')} error={'message' in errors}  helperText={errors.message?.message} />
-            </Stack>
-            <Stack alignItems='center' mt={3} >
-              <Button color="primary" variant="contained" size="large" sx={{width: '200px'}} onClick={handleSubmit(onSubmit)}>
-                送信
-              </Button>
-            </Stack>
-          </FormControl>
-        </Box>
-        <Box height="20vh"></Box>
-      </Container>
-      <Footer />
-    </main>
+      <HeadTag />
+      <main>
+        <Header />
+        <Container maxWidth='md'>
+          <Box mb={6}>
+            <PageTitle title="Contact." />
+            <Typography align='center'>
+              このページはお問い合わせページです。<br/>
+            </Typography>
+          </Box>
+          <Box>
+            <FormControl fullWidth>
+              <Stack spacing={3} direction='column' >
+                <TextField variant="outlined" required label="お名前" {...register('name')} error={'name' in errors}  helperText={errors.name?.message} />
+                <TextField variant="outlined" required label="メールアドレス" type="email" {...register('email')} error={'email' in errors}  helperText={errors.email?.message} />
+                <TextField variant="outlined" required label="問い合わせ内容" multiline rows={6} {...register('message')} error={'message' in errors}  helperText={errors.message?.message} />
+              </Stack>
+              <Stack alignItems='center' mt={3} >
+                <Button color="primary" variant="contained" size="large" sx={{width: '200px'}} onClick={handleSubmit(onSubmit)}>
+                  送信
+                </Button>
+              </Stack>
+            </FormControl>
+          </Box>
+          <Box height="20vh"></Box>
+        </Container>
+        <Footer />
+      </main>
     </>
   )
 };
